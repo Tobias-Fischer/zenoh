2375eb8c6185e380a9252367fa0054b6801df0ce
Tobias Fischer <info@tobiasfischer.info>
2026-09-17 08:04:29 +1000

    Revert "DEBUG: instrument pull() loop"
    This reverts commit 79869c4ff0d80f9c381f622805ed10597b76af0d.

--- io/zenoh-transport/src/common/pipeline.rs
+++ io/zenoh-transport/src/common/pipeline.rs
@@ -1216,7 +1216,6 @@ pub(crate) trait PipelineConsumer {
                 Ok(res) => return Some(res),
                 Err(b) => b,
             };
-            eprintln!("DEBUG pull() loop: backoff={:?}", backoff);
             // In case of writing many small messages, `recv_async()` will most likely return immediately.
             // While trying to pull from the queue, the stage_in `lock()` will most likely taken, leading to
             // a spinning behaviour while attempting to take the lock. Yield the current task to avoid
@@ -1232,12 +1231,8 @@ pub(crate) trait PipelineConsumer {
                     )
                     .await
                 }
-                None => {
-                    eprintln!("DEBUG pull() about to wait_async() UNBOUNDED");
-                    Ok(self.n_out_r().wait_async().await)
-                }
+                None => Ok(self.n_out_r().wait_async().await),
             };
-            eprintln!("DEBUG pull() backoff_wait result: ok={} err={}", backoff_wait.is_ok(), backoff_wait.is_err());
             match backoff_wait {
                 Ok(Ok(())) => {
                     // We have received a notification from the channel that some bytes are available, retry to pull.
